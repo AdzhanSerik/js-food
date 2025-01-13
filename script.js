c3f49173f8d517b36464a9f57d7b4ae8d6a7fd5f
AdzhanSerik <adzhan.serik@gmail.com>
2025-01-13 21:20:24 +0500

    dasdasd

--- script.js
+++ script.js
@@ -1,125 +1,11 @@
-const pizzas = [
-    {
-        "id": 1,
-        "imageUrl": "/pizzas/1.svg",
-        "name": "Сырная",
-        "types": [0],
-        "sizes": [26, 40],
-        "price": 245,
-        "category": 1,
-        "rating": 6
-    },
-    {
-        "id": 2,
-        "imageUrl": "/pizzas/2.svg",
-        "name": "Цыпленок барбекю",
-        "types": [0],
-        "sizes": [26, 40],
-        "price": 295,
-        "category": 1,
-        "rating": 4
-    },
-    {
-        "id": 3,
-        "imageUrl": "/pizzas/3.svg",
-        "name": "Кисло-сладкий цыпленок",
-        "types": [1],
-        "sizes": [26, 30, 40],
-        "price": 275,
-        "category": 2,
-        "rating": 2
-    },
-    {
-        "id": 4,
-        "imageUrl": "/pizzas/4.svg",
-        "name": "Чизбургер-пицца",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 415,
-        "category": 3,
-        "rating": 8
-    },
-    {
-        "id": 5,
-        "imageUrl": "/pizzas/5.svg",
-        "name": "Крэйзи пепперони",
-        "types": [0],
-        "sizes": [30, 40],
-        "price": 580,
-        "category": 2,
-        "rating": 2
-    },
-    {
-        "id": 6,
-        "imageUrl": "/pizzas/6.svg",
-        "name": "Пепперони",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 675,
-        "category": 1,
-        "rating": 9
-    },
-    {
-        "id": 7,
-        "imageUrl": "/pizzas/7.svg",
-        "name": "Маргарита",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 450,
-        "category": 4,
-        "rating": 10
-    },
-    {
-        "id": 8,
-        "imageUrl": "/pizzas/8.svg",
-        "name": "Четыре сезона",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 395,
-        "category": 5,
-        "rating": 10
-    },
-    {
-        "id": 9,
-        "imageUrl": "/pizzas/8.svg",
-        "name": "Четыре сезона",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 395,
-        "category": 5,
-        "rating": 10
-    },
-    {
-        "id": 10,
-        "imageUrl": "/pizzas/8.svg",
-        "name": "Четыре сезона",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 395,
-        "category": 5,
-        "rating": 10
-    },
-    {
-        "id": 11,
-        "imageUrl": "/pizzas/8.svg",
-        "name": "Четыре сезона",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 395,
-        "category": 5,
-        "rating": 10
-    },
-    {
-        "id": 12,
-        "imageUrl": "/pizzas/8.svg",
-        "name": "Четыре сезона",
-        "types": [0, 1],
-        "sizes": [26, 30, 40],
-        "price": 395,
-        "category": 5,
-        "rating": 10
-    }
-]
+const API = 'https://272048b109ede573.mokky.dev/pizzas'
+const search = document.querySelector('.search')
+// const pizzas = ''
+
+search.oninput = () => {
+    const searchValue = search.value
+    renderPizzas(searchValue)
+}
 
 const cartButton = document.querySelector('.cart-button')
 cartButton.addEventListener('click', () => {
@@ -129,10 +15,6 @@ cartButton.addEventListener('click', () => {
 document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.cart').classList.add('hidden')
 })
-
-
-const res = pizzas.find(pizza => pizza.id == 2)
-console.log(res)
 
 const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые']
 const categoriesList = document.querySelector('.categories')
@@ -166,9 +48,16 @@ renderCategories(0)
 
 const pizzaList = document.querySelector('#pizzaList')
 
-renderPizzas(pizzas)
+renderPizzas('')
 
-function renderPizzas(pizzas) {
+async function getPizzas(searchValue) {
+    const response = await fetch(API + `?name=*${searchValue}*`)
+    const data = await response.json()
+    return data
+}
+
+async function renderPizzas(searchValue) {
+    const pizzas = await getPizzas(searchValue)
     pizzaList.innerHTML = ''
     pizzas.forEach(item => {
         pizzaList.insertAdjacentHTML('beforeend', `
@@ -269,3 +158,10 @@ document.querySelector('.remove-cart').addEventListener('click', () => {
 })
 
 
+const img = document.querySelector('.img')
+const listItems = document.querySelector('.list-items')
+
+document.querySelector('.tog').addEventListener('click', () => {
+    listItems.classList.toggle('whatch')
+    img.classList.toggle('rotate-180')
+})  
